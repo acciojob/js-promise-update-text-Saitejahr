fdf4fbb03e3cac45515e16ee0d37dc6a202485b4
SAI TEJA H R <98202897+Saitejahr@users.noreply.github.com>
2023-03-17 14:01:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your JS code here. If required.
+let output = document.getElementById("output");
+function Hello(){
+	return new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			resolve("Hello, world!");
+		}, 1000);
+	});
+}
+
+Hello().then((data) => {
+   output.textContent = data;
+});
